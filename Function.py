3dec0936c0b2d9caf0b55be1cfeeff338b27241e
vidya29 <vidya@ripple.com>
2017-01-24 14:38:24 -0800

    test again

--- Function.py
+++ Function.py
@@ -1,5 +1,10 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 def is_prime(number):
-    """Return True if *number* is prime."""
+    logger.info("Return True if *number* is prime.")
     for element in range(2, number):
         if number % element == 0:
             return False
@@ -7,7 +12,7 @@ def is_prime(number):
     return True
 
 def print_next_prime(number):
-    """Print the closest prime number larger than *number*."""
+    logger.info("Print the closest prime number larger than *number*.")
     index = number
     while True:
         index += 1
--- Test.py
+++ Test.py
@@ -1,11 +1,15 @@
-import unittest
+import unittest, logging
 from Function import is_prime
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class PrimesTestCase(unittest.TestCase):
     """Tests for `Function.py`."""
 
     def test_is_five_prime(self):
-        """Is five successfully determined to be prime?"""
+        logger.info("\n************** Is 5 prime ******************")
+
         self.assertTrue(is_prime(5))
 
 if __name__ == '__main__':
